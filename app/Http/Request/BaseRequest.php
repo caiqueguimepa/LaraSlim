21fcac456e45065e9de4237a0013c8be4b5ab3f5
caiqueguimepa <caique@guimepa.com.br>
2025-04-08 15:38:41 -0300

    feat: criando Basequest classe

---
+++ app/Http/Request/BaseRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LaraSlim\Http\Request;
+
+use Illuminate\Validation\Factory;
+
+abstract class BaseRequest
+{
+    protected array $data = [];
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function validate()
+    {
+        $validator = app(Factory::class);
+
+        $validation = $validator->make(
+            $this->data,
+            $this->rules(),
+            $this->messages()
+        );
+
+        return $validation;
+    }
+
+    abstract protected function rules(): array;
+
+    protected function messages(): array
+    {
+        return [];
+    }
+}
